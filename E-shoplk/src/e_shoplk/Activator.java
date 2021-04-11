5a63836c346857b6ac83146192dc860b4a85e914
shalithacode <shalithalakruwan1998@gmail.com>
2021-04-11 17:34:30 +0530

    E-shoplk Activator created

--- E-shoplk/src/e_shoplk/Activator.java
+++ E-shoplk/src/e_shoplk/Activator.java
@@ -2,21 +2,20 @@ package e_shoplk;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
 
-	private static BundleContext context;
+	ServiceRegistration shopServiceRegistration;
 
-	static BundleContext getContext() {
-		return context;
+	public void start(BundleContext context) throws Exception {
+		System.out.println("Shop start");
+		Shop publisherShopService = new Shop();
+		shopServiceRegistration = context.registerService(Shop.class.getName(), publisherShopService, null);
 	}
 
-	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Shop stop");
+		shopServiceRegistration.unregister();
 	}
-
-	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
-	}
-
 }
