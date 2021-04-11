81e4255fe9c1c8b50a268789eb945c26204b861e
shalithacode <shalithalakruwan1998@gmail.com>
2021-04-11 17:08:04 +0530

    Item model class created

---
+++ E-shoplk/src/e_shoplk/Item.java
@@ -0,0 +1,34 @@
+package e_shoplk;
+
+public class Item {
+
+	private String pNname;
+	private double pPrice;
+	private int pQty;
+	public Item(String pNname, double pPrice, int pQty) {
+		
+		this.pNname = pNname;
+		this.pPrice = pPrice;
+		this.pQty = pQty;
+	}
+	public String getpNname() {
+		return pNname;
+	}
+	public void setpNname(String pNname) {
+		this.pNname = pNname;
+	}
+	public double getpPrice() {
+		return pPrice;
+	}
+	public void setpPrice(double pPrice) {
+		this.pPrice = pPrice;
+	}
+	public int getpQty() {
+		return pQty;
+	}
+	public void setpQty(int pQty) {
+		this.pQty = pQty;
+	}
+	
+	
+}
