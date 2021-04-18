7472632aa1d77c7f7be776afaa7cc178980ecff0
shalithacode <shalithalakruwan1998@gmail.com>
2021-04-18 21:02:31 +0530

    Refactored Shop bundle

--- E-shoplk/src/e_shoplk/Activator.java
+++ E-shoplk/src/e_shoplk/Activator.java
@@ -10,15 +10,20 @@ public class Activator implements BundleActivator {
 	ServiceRegistration loginServiceRegistration;
 
 	public void start(BundleContext context) throws Exception {
+		
+		//Shop service registration 
 		System.out.println("Shop plugin activated");
 		Ishop publisherShopService = new Shop();
 		shopServiceRegistration = context.registerService(Shop.class.getName(), publisherShopService, null);
-		
+
+		//Login service registration 
 		Login publisherLoginService = new Login();
 		loginServiceRegistration = context.registerService(Login.class.getName(), publisherLoginService, null);
 	}
 
 	public void stop(BundleContext context) throws Exception {
+		
+		//Deactivate all the services
 		System.out.println("Shop plugin deactivated");
 		shopServiceRegistration.unregister();
 		loginServiceRegistration.unregister();
--- E-shoplk/src/e_shoplk/Shop.java
+++ E-shoplk/src/e_shoplk/Shop.java
@@ -3,39 +3,42 @@ package e_shoplk;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class Shop implements Ishop{
+public class Shop implements Ishop {
+	// Item list
 	ArrayList<Item> item = new ArrayList<>();
-	
+
 	public void addProduct(String name, double price, int qty) {
-		
+
 		item.add(new Item(name, price, qty));
 	}
-	
+
+	// Get all the items
 	public boolean viewProduct() {
-		 Iterator itr=item.iterator();  
-		 
-		 if(item.isEmpty())
-			 return false;
-		 else {
-			 System.out.println(" Item | Price | Quntity ");
-		  while(itr.hasNext()){  
-		    Item product=(Item)itr.next();  
-		    System.out.println(" " + product.getpNname()+" | "+product.getpPrice()+" | "+product.getpQty());  
-		  }
-		return true; 
-		 }
-		
+		Iterator itr = item.iterator();
+
+		if (item.isEmpty())
+			return false;
+		else {
+			System.out.println(" Item | Price | Quntity ");
+			while (itr.hasNext()) {
+				Item product = (Item) itr.next();
+				System.out.println(" " + product.getpNname() + " | " + product.getpPrice() + " | " + product.getpQty());
+			}
+			return true;
+		}
 	}
+
+	// Delete the item
 	public boolean deleteProduct(String prod) {
-		 Iterator itr=item.iterator();  
-		 while(itr.hasNext()){  
-			Item product=(Item)itr.next();
-			if(prod.equals(product.getpNname())) {
-				
+		Iterator itr = item.iterator();
+		while (itr.hasNext()) {
+			Item product = (Item) itr.next();
+			if (prod.equals(product.getpNname())) {
+
 				item.remove(product);
 				return true;
 			}
-	}
-		 return false;
+		}
+		return false;
 	}
 }
